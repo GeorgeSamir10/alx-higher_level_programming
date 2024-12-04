fd8d9d3a16211024a22587dc998d5eaacf77ace4
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-12-04 23:29:22 +0200

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines the class named Square that defines a square.
+"""
+
+
+class Square:
+    """
+    This is an empty class named Square that defines a square.
+    """
+    def __init__(self, size):
+        """
+        This method initiates an object instance of class Square.
+        Args:
+            size (int): size of the square to be initiated.
+        """
+        self.__size = size
